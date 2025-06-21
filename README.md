ed360c74bd9708547692924ade331b0b980d484b
Ramz5154 <ramz5154@gmail.com>
2025-06-20 21:37:21 -0400

    getting started and setting up

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# VectorLab
+# VectorLab 
+
+## getting started 
+download glfw3 and glad (i did it through vcpkg)
+
+then go to Configuration Properties > Linker > Input
+
+Click Additional Dependencies add opengl32.lib
+
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-	std::cout << "test";
+    if (!glfwInit()) return -1;
 
-	return 0;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "VectorLab", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
